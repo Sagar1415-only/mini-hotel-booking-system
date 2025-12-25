eb21fe4ef17e2de25134e6d478f57fdc5a90e9ff
Sagar.M.S <sag188392@gmail.com>
2025-12-25 18:40:13 +0530

    Update MongoDB connection options

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/hotelDB")
+mongoose
+  .connect("mongodb://127.0.0.1:27017/hotelDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+  .catch(err => console.error(err));
