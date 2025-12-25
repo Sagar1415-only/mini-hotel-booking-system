04884d6d904c4608c000eb645c33e878c8278cb7
Sagar.M.S <sag188392@gmail.com>
2025-12-25 18:43:30 +0530

    Remove authRoutes from server.js
    Removed authentication routes from the server.

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ require("./db");
 
 const roomRoutes = require("./routes/roomRoutes");
 const bookingRoutes = require("./routes/bookingRoutes");
-const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 app.use(cors());
@@ -12,7 +11,6 @@ app.use(express.json());
 
 app.use("/rooms", roomRoutes);
 app.use("/bookings", bookingRoutes);
-app.use("/auth", authRoutes);
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
